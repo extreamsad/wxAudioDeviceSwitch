0fcb2064246266f7aaa6acba9a7d70f74b177e16
extream <extream.sad@mail.ru>
2022-09-16 16:49:47 +0300

    Any additions

--- ads/MyTaskBarIcon.cpp
+++ ads/MyTaskBarIcon.cpp
@@ -23,9 +23,12 @@ void MyTaskBarIcon::OnMenuOptions(wxCommandEvent &)
 	m_main_form->Show();
 }
 
-void MyTaskBarIcon::OnShowPopupMenu(wxUpdateUIEvent & event)
-{		
-	//event.Check(true);
+void MyTaskBarIcon::OnMenuItemUpdateUI(wxUpdateUIEvent & event)
+{	
+	if (!event.GetChecked()) 
+	{
+	}
+	//event.Check(*bflag);
 }
 
 void MyTaskBarIcon::OnMenuExit(wxCommandEvent &)
@@ -46,14 +49,23 @@ wxMenu * MyTaskBarIcon::CreatePopupMenu()
 	wxMenu *popup_menu = new wxMenu();
 	wxMenuItem *menu_close = new wxMenuItem(popup_menu, wxID_ANY, wxString(wxT("Exit")), wxEmptyString, wxITEM_NORMAL);
 	wxMenuItem *menu_options = new wxMenuItem(popup_menu, wxID_ANY, wxString(wxT("Options")), wxEmptyString, wxITEM_NORMAL);
-	
+
+	m_devices.reset(new std::vector<WAVEOUTCAPS>());
+	bool bDefDev = true;
+
 	for (auto &d : MainForm::GetAudioPlaybackDevices())
 	{
-		wxString dev_name{ d.szPname };
+		m_devices->emplace_back(d);
+
+		wxString dev_name{ d.szPname };		
 		wxMenuItem *devs_menu = new wxMenuItem(popup_menu, wxID_ANY, dev_name, wxEmptyString, wxITEM_CHECK);
+
 		popup_menu->Append(devs_menu);
 		popup_menu->Bind(wxEVT_COMMAND_MENU_SELECTED, wxCommandEventHandler(MyTaskBarIcon::OnMenuAudioDevice), this, devs_menu->GetId());
-		this->Connect(devs_menu->GetId(), wxEVT_UPDATE_UI, wxUpdateUIEventHandler(MyTaskBarIcon::OnShowPopupMenu));
+		this->Connect(devs_menu->GetId(), wxEVT_UPDATE_UI, wxUpdateUIEventHandler(MyTaskBarIcon::OnMenuItemUpdateUI));
+		devs_menu->Check(bDefDev);		
+
+		bDefDev ? bDefDev = false : bDefDev;
 	}
 	popup_menu->AppendSeparator();
 	popup_menu->Append(menu_options);
--- ads/MyTaskBarIcon.h
+++ ads/MyTaskBarIcon.h
@@ -8,13 +8,14 @@ class MyTaskBarIcon :
 	public wxTaskBarIcon
 {	
 	MainForm *m_main_form = nullptr;	
+	std::shared_ptr<std::vector<WAVEOUTCAPS>> m_devices;
 public:
 	MyTaskBarIcon(MainForm* parent);
 	~MyTaskBarIcon();
 
 	void OnLeftButtonDClick(wxTaskBarIconEvent&);
 	void OnMenuOptions(wxCommandEvent&);
-	void OnShowPopupMenu(wxUpdateUIEvent& event);
+	void OnMenuItemUpdateUI(wxUpdateUIEvent& event);
 	void OnMenuExit(wxCommandEvent&);
 	void OnMenuSetNewIcon(wxCommandEvent&);
 	void OnMenuAudioDevice(wxCommandEvent&);
